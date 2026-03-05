042cf73cd858f1860e5769d178dc5ee5b92611da
Omkar Rajguru <72383143+orajguru@users.noreply.github.com>
2026-03-05 17:12:24 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,52 +2,96 @@
 import pandas as pd
 from lxml import etree
 
-METADATA_COLUMNS = [
-"req","fieldid","fieldtype","name","relatedtype","hidechar","resetonsave",
-"searchable","hideformat","iseditable","listingtype","listingid","valign",
-"showlabel","istileview","showlabelposition","maskingid","maskingtext",
-"highlight","trackhist","hide","hidecontroltype","readonly","maxattachment",
-"folderid","readonlytype","showtodropdown","renderingNameMode",
-"requiredNameMode","salutationReq","suffixReq","isAutoComplete",
-"isAutoSizeAlwd","controltype","rendering","evaluatorid","disableCopyPaste",
-"searchop","isNewAlwd","colspan","rowspan","isuppercase"
-]
+
+def get_parent_attr(element, tag_name):
+    """Find nearest ancestor with given tag"""
+    parent = element.getparent()
+    while parent is not None:
+        if parent.tag.lower() == tag_name:
+            return parent.get("name")
+        parent = parent.getparent()
+    return "UNKNOWN"
+
 
 def parse_xml(file):
 
     tree = etree.parse(file)
     root = tree.getroot()
 
-    rows = []
+    fields = root.findall(".//field")
 
-    for tab in root.xpath(".//tab"):
-        tab_name = tab.get("name")
+    if not fields:
+        return pd.DataFrame()
 
-        for section in tab.xpath(".//section"):
-            section_name = section.get("name")
+    # Dynamically collect ALL metadata attributes
+    metadata_keys = set()
 
-            row_counter = 1
+    for f in fields:
+        for k in f.attrib.keys():
+            metadata_keys.add(k)
 
-            for row in section.xpath(".//row"):
-                col_counter = 1
+    metadata_keys = sorted(list(metadata_keys))
 
-                for field in row.xpath(".//field"):
-                    record = {
-                        "Tab_Name": tab_name,
-                        "Section_Name": section_name,
-                        "Row_ID": row_counter,
-                        "Column_ID": col_counter
-                    }
+    records = []
 
-                    for meta in METADATA_COLUMNS:
-                        val = field.get(meta)
-                        record[meta] = val if val is not None else "NULL"
+    section_row_map = {}
 
-                    rows.append(record)
+    for field in fields:
 
-                    col_counter += 1
+        tab_name = get_parent_attr(field, "tab")
+        section_name = get_parent_attr(field, "section")
 
-                row_counter += 1
+        # Detect row
+        row_node = None
+        parent = field.getparent()
+        while parent is not None:
+            if parent.tag.lower() == "row":
+                row_node = parent
+                break
+            parent = parent.getparent()
 
-    df = pd.DataFrame(rows)
+        row_id = None
+        col_id = None
+
+        if row_node is not None:
+
+            section_key = (tab_name, section_name)
+
+            if section_key not in section_row_map:
+                section_row_map[section_key] = {}
+
+            row_map = section_row_map[section_key]
+
+            if row_node not in row_map:
+                row_map[row_node] = len(row_map) + 1
+
+            row_id = row_map[row_node]
+
+            # Determine column index
+            cols = row_node.findall(".//col")
+            col_id = 1
+
+            if cols:
+                for i, col in enumerate(cols, start=1):
+                    if field in col.iter():
+                        col_id = i
+                        break
+        else:
+            row_id = 1
+            col_id = 1
+
+        record = {
+            "Tab_Name": tab_name,
+            "Section_Name": section_name,
+            "Row_ID": row_id,
+            "Column_ID": col_id
+        }
+
+        for key in metadata_keys:
+            record[key] = field.attrib.get(key, "NULL")
+
+        records.append(record)
+
+    df = pd.DataFrame(records)
+
     return df
